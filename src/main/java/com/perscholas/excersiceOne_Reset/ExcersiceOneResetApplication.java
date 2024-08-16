74dbe36912153379fe22425974df4923582bfde5
Rikimo23 <162639270+Rikimo23@users.noreply.github.com>
2024-08-16 11:22:40 -0400

    finito

---
+++ src/main/java/com/perscholas/excersiceOne_Reset/ExcersiceOneResetApplication.java
@@ -0,0 +1,13 @@
+package com.perscholas.excersiceOne_Reset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcersiceOneResetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcersiceOneResetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/perscholas/excersiceOne_Reset/ExcersiceOneResetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perscholas.excersiceOne_Reset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcersiceOneResetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
